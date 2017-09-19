c78f03b434a91f8bf65de560895171f0bae875f7
David Hockenberry <dhockenberry@gmail.com>
2017-09-18 20:19:23 -0700

    pledge report debugging

--- app/mailers/pcocore_mailer.rb
+++ app/mailers/pcocore_mailer.rb
@@ -1,14 +1,6 @@
 class PcocoreMailer < ApplicationMailer
-  default from: "smtpout@community-christian.us"
-
-#  def sample_email(user)
-#    @user = user
-#    mail(to: @user.email, subject: 'Sample Email')
-#    mail(to: "dhockenberry@gmail.com", subject: 'Sample Email')
-# end
 
   def send_email(eml_address,eml_subject,eml_body)
-#    @user = user
     mg_client = Mailgun::Client.new ENV['MAILGUN_API']
     message_params = {:from    => ENV['MAILGUN_USERNAME'],
                       :to      => eml_address,
