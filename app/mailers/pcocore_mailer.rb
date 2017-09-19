568ec4402cc0b8bdea3541f11f8fbd4190c71410
David Hockenberry <dhockenberry@gmail.com>
2017-09-18 20:29:14 -0700

    pledge report debugging

--- app/mailers/pcocore_mailer.rb
+++ app/mailers/pcocore_mailer.rb
@@ -1,12 +1,14 @@
 class PcocoreMailer < ApplicationMailer
 
+settings = Setting.x
+
   def send_email(eml_address,eml_subject,eml_body)
-    mg_client = Mailgun::Client.new ENV['MAILGUN_API']
-    message_params = {:from    => ENV['MAILGUN_USERNAME'],
+    mg_client = Mailgun::Client.new settings.mailgun_api
+    message_params = {:from    => settings.mailgun_username,
                       :to      => eml_address,
                       :subject => eml_subject,
                       :text    => eml_body
                     }
-    mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
+    mg_client.send_message settings.mailgun_domain, message_params
   end
 end
