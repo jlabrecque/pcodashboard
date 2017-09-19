70a73d16d8afa652d6264740367216a21ec22aba
David Hockenberry <dhockenberry@gmail.com>
2017-09-18 20:39:49 -0700

    pledge report debugging

--- lib/mail-test.rb
+++ lib/mail-test.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 
 #If cli parms not passed, so update from last offset ...
-  if ARGV.count == 1 and  == "update"
+  if ARGV.count == 1
     eml_subject = "Another test email"
     eml_body    = "This is the body of the sample email I am contructing and want to expand this a bit.... blah, blah, blah"
     PcocoreMailer.send_email(ARGV.first,eml_subject,eml_body).deliver
