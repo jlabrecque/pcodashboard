e4741dd74838469a780035f4ad3bef57d6b0d0b5
David Hockenberry <dhockenberry@gmail.com>
2017-09-18 20:40:36 -0700

    pledge report debugging

--- lib/mail-test.rb
+++ lib/mail-test.rb
@@ -6,6 +6,9 @@ require 'rubygems'
     eml_body    = "This is the body of the sample email I am contructing and want to expand this a bit.... blah, blah, blah"
     PcocoreMailer.send_email(ARGV.first,eml_subject,eml_body).deliver
   else
+    puts "======================================================================"
     puts "mail-test requires at least one parameter for the target email address"
     puts "Example: rails runner lib/mail-test.rb <targetemailaddress>"
+    puts "======================================================================"
+
   end
