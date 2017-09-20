6f71c81462ac7fb6b44bf1bf1d8f1dd8e2af8b1c
David Hockenberry <dhockenberry@gmail.com>
2017-09-19 20:49:33 -0700

    workflow, abilities fixes

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "PCOpledge"
+  config.site_title = "PCOdashboard"
   config.register_javascript 'https://www.google.com/jsapi'
 
   # Set the link url for the title. For example, to take
