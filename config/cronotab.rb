8ba8e031cebb9b1f61149c1efc3024c3568c9497
David Hockenberry <dhockenberry@gmail.com>
2017-10-28 07:56:23 -0700

    Crono fix

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -1,13 +1,13 @@
 # cronotab.rb — Crono configuration file
 
-$checkinsload = Cron.checkins[0]
-$donationsload = Cron.donations[0]
-$peopleload = Cron.people[0]
+$checkinsload = Cron.checkins.first
+$donationsload = Cron.donation.first
+$peopleload = Cron.people.first
 
-$servicesload = Cron.services[0]
+$servicesload = Cron.services.first
 
-$mailchimpload = Cron.mailchimp[0]
-$geocodedaily = Cron.geocode[0]
+$mailchimpload = Cron.mailchimp.first
+$geocodedaily = Cron.geocode.first
 
 
 class PeopleLoad < ActiveJob::Base
@@ -56,11 +56,11 @@ if $checkinsload.enabled
   Crono.perform(CheckinsLoad).every 1.days, at: {hour: $checkinsload.hour, min: $checkinsload.minute}
 end
 if $servicesload.enabled
-  Crono.perform(ServicetypeLoad).every 1.days, at: {hour: $servicesload.hour, min: $servicesload.minute}
+  Crono.perform(ServicesLoad).every 1.days, at: {hour: $servicesload.hour, min: $servicesload.minute}
 end
 
 if $mailchimpload.enabled
-  Crono.perform(MailchimplistsLoad).every 1.days, at: {hour: $mailchimpload.hour, min: $mailchimpload.minute}
+  Crono.perform(MailchimpLoad).every 1.days, at: {hour: $mailchimpload.hour, min: $mailchimpload.minute}
 end
 if $geocodedaily.enabled
   Crono.perform(GeocodeDaily).every 1.days, at: {hour: $geocodedaily.hour, min: $geocodedaily.minute}
