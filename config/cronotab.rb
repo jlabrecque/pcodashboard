4127c78d5ecfebcc663f3cdfd5687d1c672893a3
David Hockenberry <dhockenberry@gmail.com>
2017-10-28 07:33:00 -0700

    Fixed Cronotab entries

--- config/cronotab.rb
+++ config/cronotab.rb
@@ -4,17 +4,15 @@ $checkinsload = Cron.checkins[0]
 $donationsload = Cron.donations[0]
 $peopleload = Cron.people[0]
 
-$servicetypeload = Cron.servicetypes[0]
-$plansload = Cron.plans[0]
-$teammembersload = Cron.teammembers[0]
+$servicesload = Cron.servicetypes[0]
 
-$mailchimplistsload = Cron.mailchimplists[0]
+$mailchimpload = Cron.mailchimplists[0]
 $geocodedaily = Cron.geocode[0]
 
 
-class CheckinsLoad < ActiveJob::Base
+class PeopleLoad < ActiveJob::Base
   def perform
-    load $checkinsload.script_name
+    load $peopleload.script_name
   end
 end
 
@@ -24,63 +22,45 @@ class DonationsLoad < ActiveJob::Base
   end
 end
 
-class PeopleLoad < ActiveJob::Base
+class CheckinsLoad < ActiveJob::Base
   def perform
-    load $peopleload.script_name
+    load $checkinsload.script_name
   end
 end
 
-class ServicetypeLoad < ActiveJob::Base
+class ServicesLoad < ActiveJob::Base
   def perform
-    load $servicetypeload.script_name
-  end
-end
-class PlansLoad < ActiveJob::Base
-  def perform
-    load $plansload.script_name
-  end
-end
-class TeammembersLoad < ActiveJob::Base
-  def perform
-    load $teammembersload.script_name
+    load $servicesload.script_name
   end
 end
 
-class MailchimplistsLoad < ActiveJob::Base
+class MailchimpLoad < ActiveJob::Base
   def perform
-    load $mailchimplistsload.script_name
+    load $mailchimpload.script_name
   end
 end
+
 class GeocodeDaily < ActiveJob::Base
   def perform
     load $geocodedaily.script_name
   end
 end
 
-if $checkinsload.enabled
-  Crono.perform(CheckinsLoad).every 1.days, at: {hour: $checkinsload.hour, min: $checkinsload.minute}
-end
-
-if $donationsload.enabled
-  Crono.perform(DonationsLoad).every 1.days, at: {hour: $donationsload.hour, min: $donationsload.minute}
-end
-
 if $peopleload.enabled
   Crono.perform(PeopleLoad).every 1.days, at: {hour: $peopleload.hour, min: $peopleload.minute}
 end
+if $donationsload.enabled
+  Crono.perform(DonationsLoad).every 1.days, at: {hour: $donationsload.hour, min: $donationsload.minute}
+end
+if $checkinsload.enabled
+  Crono.perform(CheckinsLoad).every 1.days, at: {hour: $checkinsload.hour, min: $checkinsload.minute}
+end
+if $servicesload.enabled
+  Crono.perform(ServicetypeLoad).every 1.days, at: {hour: $servicesload.hour, min: $servicesload.minute}
+end
 
-if $servicetypeload.enabled
-  Crono.perform(ServicetypeLoad).every 1.days, at: {hour: $servicetypeload.hour, min: $servicetypeload.minute}
-end
-if $plansload.enabled
-  Crono.perform(PlansLoad).every 1.days, at: {hour: $plansload.hour, min: $plansload.minute}
-end
-if $teammembersload.enabled
-  Crono.perform(TeammembersLoad).every 1.days, at: {hour: $teammembersload.hour, min: $teammembersload.minute}
-end
-
-if $mailchimplistsload.enabled
-  Crono.perform(MailchimplistsLoad).every 1.days, at: {hour: $mailchimplistsload.hour, min: $mailchimplistsload.minute}
+if $mailchimpload.enabled
+  Crono.perform(MailchimplistsLoad).every 1.days, at: {hour: $mailchimpload.hour, min: $mailchimpload.minute}
 end
 if $geocodedaily.enabled
   Crono.perform(GeocodeDaily).every 1.days, at: {hour: $geocodedaily.hour, min: $geocodedaily.minute}
