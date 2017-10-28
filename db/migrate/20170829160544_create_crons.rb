e088671ed9e12d5e7b825329229fe35e53ee7758
David Hockenberry <dhockenberry@gmail.com>
2017-10-27 21:06:28 -0700

    Update Cron job initial entries for reload

--- db/migrate/20170829160544_create_crons.rb
+++ db/migrate/20170829160544_create_crons.rb
@@ -16,10 +16,8 @@ class CreateCrons < ActiveRecord::Migration[5.0]
   prime = Cron.create(:job_name => "PeopleReload", :script_name => "lib/people-dbload.rb", :parameters => "update", :description => "Weekly full People dB sync",:dow => "Sun", :recurring => "Weekly",:hour => 22,:minute => 00,:enabled => FALSE)
   prime = Cron.create(:job_name => "DonationsLoad", :script_name => "lib/donations-dbload.rb", :parameters => "", :description => "Nightly incremently Donations dB sync",:dow => "", :recurring => "Daily",:hour => 1,:minute => 00,:enabled => FALSE)
   prime = Cron.create(:job_name => "CheckinsLoad", :script_name => "lib/checkins-dbload.rb", :parameters => "", :description => "Nightly incremently Checkins dB sync",:dow => "", :recurring => "Daily",:hour => 2,:minute => 00,:enabled => FALSE)
-  prime = Cron.create(:job_name => "ServicetypeLoad", :script_name => "lib/servicetypes-dbload.rb", :parameters => "", :description => "Nightly incremently ServiceType dB sync",:dow => "", :recurring => "Daily",:hour => 3,:minute => 00,:enabled => FALSE)
-  prime = Cron.create(:job_name => "PlansLoad", :script_name => "lib/plans-dbload.rb", :parameters => "", :description => "Nightly incremently Plans dB sync",:dow => "", :recurring => "Daily",:hour => 3,:minute => 15,:enabled => FALSE)
-  prime = Cron.create(:job_name => "TeammembersLoad", :script_name => "lib/teammember-dbload.rb", :parameters => "", :description => "Nightly incremently TeamMembers dB sync",:dow => "", :recurring => "Daily",:hour => 3,:minute => 30,:enabled => FALSE)
-  prime = Cron.create(:job_name => "MailchimplistsLoad", :script_name => "lib/mailchimplist-dbload.rb", :parameters => "", :description => "Nightly Mailchimp List sync",:dow => "", :recurring => "Daily",:hour => 5,:minute => 00,:enabled => FALSE)
+  prime = Cron.create(:job_name => "ServicesLoad", :script_name => "lib/service-dbload.rb", :parameters => "", :description => "Nightly incremently Service dB sync",:dow => "", :recurring => "Daily",:hour => 3,:minute => 00,:enabled => FALSE)
+  prime = Cron.create(:job_name => "MailchimpLoad", :script_name => "lib/mailchimp-dbload.rb", :parameters => "", :description => "Nightly Mailchimp sync",:dow => "", :recurring => "Daily",:hour => 5,:minute => 00,:enabled => FALSE)
   prime = Cron.create(:job_name => "GeocodeDaily", :script_name => "lib/geocode-dbload.rb", :parameters => "", :description => "Nightly Geocode pass -- split alphabetically",:dow => "", :recurring => "Daily",:hour => 6,:minute => 00,:enabled => FALSE)
 
 end
